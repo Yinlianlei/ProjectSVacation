da4b54df0996a3e5edae611beb2413de1895bf0a
Yinlianlei <1114561520@qq.com>
2022-07-04 16:33:18 +0800

    delete

--- old/static/AI.js
+++
@@ -1,7 +0,0 @@
-function listenForTAIText(){
-    console.log("add listen");
-    var text = document.getElementById("text_response");
-    text.addEventListener("input",function(){
-        text.style.height = 300;
-    });
-}
--- old/test.py
+++
@@ -1,7 +0,0 @@
-import re
-
-if __name__ == "__main__":
-    a = ["房事头疼症","龟头疼","受凉头疼","肺部转移"]
-
-    for i in a:
-        print(re.findall("[头疼]+",i))
